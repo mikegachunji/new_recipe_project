df354db545f5f62a92f83604aaf322706edc6cd2
Michael Gachunji <mikegachunji@gmail.com>
2018-07-27 02:38:15 +0300

    Added use of instance folder

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 # run.py
  
-# Add your settings here... this is a temporary location, as the settings for a Flask app
-# should be stored separate from your main program.
-DEBUG = True
- 
+
 from nrp_env import app
  
 if __name__ == "__main__":
