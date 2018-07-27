b8bc2280f20eba7582bf03c18d9841b218fb9a4d
Michael Gachunji <mikegachunji@gmail.com>
2018-07-27 03:28:56 +0300

    Added use of templates and Bootstrap

--- nrp_env/recipes/views.py
+++ nrp_env/recipes/views.py
@@ -1,10 +1,9 @@
-# project/recipes/views.py
- 
 #################
 #### imports ####
 #################
  
 from flask import render_template, Blueprint
+from nrp_env.models import Recipe
  
  
 ################
@@ -20,4 +19,5 @@ recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
  
 @recipes_blueprint.route('/')
 def index():
-    return render_template('index.html')
+    all_recipes = Recipe.query.all()
+    return render_template('recipes.html', recipes=all_recipes)
